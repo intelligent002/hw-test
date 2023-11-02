e0f04f17f959ee11c0a67882c3101437983b22c5
Arkady Rosenberg <arkadyr@rewire.to>
2023-11-02 02:05:46 +0200

    working locally

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -3,21 +3,126 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strings"
+	"unicode"
+	"unicode/utf8"
 )
 
-var ErrStringInvalid = errors.New("string is invalid")
-var ErrStringEmpty = errors.New("string is empty")
+var ErrInvalidString = errors.New("string is invalid")
+var ErrEmptyString = errors.New("string is empty")
+
+func getChar(input string, offset int) (rune, int, bool) {
+	char, widthCurrent := utf8.DecodeRuneInString(input[offset:])
+
+	if string(char) == "\\" {
+		var widthNext int
+		char, widthNext = utf8.DecodeRuneInString(input[offset+widthCurrent:])
+		return char, widthCurrent + widthNext, true
+	}
+
+	return char, widthCurrent, false
+}
 
 func Unpack(input string) (string, error) {
-	// Place your code here.
+
+	// init result
+	var result string = ""
+	var runeCurrent, runeNext rune
+	var index, widthCurrent, widthNext int
+	var escapedCurrent, escapedNext bool
+
+	// validate empty input
 	if len(input) == 0 {
-		return "", ErrStringEmpty
+		return result, ErrEmptyString
 	}
-	return "", nil
+
+	// iterate over the string by runes
+	for index, widthCurrent = 0, 0; index < len(input); index += widthCurrent {
+
+		// get rune current
+		runeCurrent, widthCurrent, escapedCurrent = getChar(input, index)
+
+		// get rune next
+		runeNext, widthNext, escapedNext = getChar(input, index+widthCurrent)
+
+		// decide on operation
+		switch true {
+		case unicode.IsLetter(runeCurrent) && escapedCurrent:
+			{
+				// current is an escaped letter, bad string
+				return "", ErrInvalidString
+			}
+		case unicode.IsLetter(runeCurrent) && unicode.IsLetter(runeNext):
+			{
+				// current is a letter, next is letter - add current
+				result += string(runeCurrent)
+			}
+		case unicode.IsLetter(runeCurrent) && string(runeNext) == "\\":
+			{
+				// current is a letter, next is an escaped backslash - add current
+				result += string(runeCurrent)
+			}
+		case unicode.IsLetter(runeCurrent) && unicode.IsDigit(runeNext) && escapedNext:
+			{
+				// current is a letter, next is an escaped digit - add current
+				result += string(runeCurrent)
+			}
+		case unicode.IsLetter(runeCurrent) && unicode.IsDigit(runeNext) && !escapedNext:
+			{
+				// current is a letter, next is a non escaped digit - multiply current by digit
+				result += strings.Repeat(string(runeCurrent), int(runeNext-'0'))
+				// and skip the upcoming digit
+				index += widthNext
+			}
+		case unicode.IsDigit(runeCurrent) && !escapedCurrent:
+			{
+				// current is a non escaped digit, bad string
+				return "", ErrInvalidString
+			}
+		case unicode.IsDigit(runeCurrent) && escapedCurrent && unicode.IsDigit(runeNext) && escapedNext:
+			{
+				// current is an escaped digit, next is an escaped digit - add current
+				result += string(runeCurrent)
+			}
+		case unicode.IsDigit(runeCurrent) && escapedCurrent && unicode.IsDigit(runeNext) && !escapedNext:
+			{
+				// current is an escaped digit, next is a non escaped digit - multiply current by digit
+				result += strings.Repeat(string(runeCurrent), int(runeNext-'0'))
+				// and skip the upcoming digit
+				index += widthNext
+			}
+		case string(runeCurrent) == "\\" && unicode.IsLetter(runeNext):
+			{
+				// current is an escaped backslash, next is any letter - add current
+				result += string(runeCurrent)
+			}
+		case string(runeCurrent) == "\\" && unicode.IsDigit(runeNext) && escapedNext:
+			{
+				// current is an escaped backslash, next is an escaped digit - add current
+				result += string(runeCurrent)
+			}
+		case string(runeCurrent) == "\\" && unicode.IsDigit(runeNext) && !escapedNext:
+			{
+				// current is an escaped backslash, next is a non escaped digit - multiply current by digit
+				result += strings.Repeat(string(runeCurrent), int(runeNext-'0'))
+				// and skip the upcoming digit
+				index += widthNext
+			}
+		case widthNext == 0:
+			{
+				// last rune - just add
+				result += string(runeCurrent)
+			}
+		}
+	}
+	return result, nil
 }
 
 func main() {
-	_, e := Unpack("")
-	fmt.Println(e.Error())
-
+	result, e := Unpack("a4")
+	if e != nil {
+		fmt.Println(e.Error())
+	} else {
+		fmt.Println("result is: ", result)
+	}
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -14,13 +14,12 @@ func TestUnpack(t *testing.T) {
 	}{
 		{input: "a4bc2d5e", expected: "aaaabccddddde"},
 		{input: "abccd", expected: "abccd"},
-		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
 	}
 
 	for _, tc := range tests {
@@ -34,12 +33,12 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackStringInvalid(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "12a", "aaa10b"}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
-			require.Truef(t, errors.Is(err, ErrStringInvalid), "actual error %q", err)
+			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
 		})
 	}
 }
@@ -50,7 +49,7 @@ func TestUnpackStringEmpty(t *testing.T) {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
-			require.Truef(t, errors.Is(err, ErrStringEmpty), "actual error %q", err)
+			require.Truef(t, errors.Is(err, ErrEmptyString), "actual error %q", err)
 		})
 	}
 }
