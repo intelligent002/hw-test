2ede63080928e74fb7cf2cd081fee9caa8f3bccf
Arkady Rosenberg <intelligent002@gmail.com>
2023-11-28 14:30:25 +0200

    cache

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,6 +1,13 @@
 package hw04lrucache
 
+import "sync"
+
 type Key string
+
+type cachedItem struct {
+	k Key
+	v interface{}
+}
 
 type Cache interface {
 	Set(key Key, value interface{}) bool
@@ -9,11 +16,52 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+	mutex    *sync.Mutex
+}
+
+func (c *lruCache) GCLast() {
+	if c.queue.Len() > c.capacity {
+		lastItem := c.queue.Back()
+		delete(c.items, lastItem.Value.(cachedItem).k)
+		c.queue.Remove(lastItem)
+	}
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	item, isSet := c.items[key]
+	if isSet {
+		c.queue.Remove(item)
+	}
+	c.items[key] = c.queue.PushFront(cachedItem{k: key, v: value})
+
+	c.GCLast()
+	return isSet
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	item, isSet := c.items[key]
+	if isSet != true {
+		return nil, false
+	}
+	c.queue.MoveToFront(item)
+	return item.Value.(cachedItem).v, true
+}
+
+func (c *lruCache) Clear() {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	c.queue = NewList()
+	c.items = make(map[Key]*ListItem, c.capacity)
 }
 
 func NewCache(capacity int) Cache {
@@ -21,5 +69,6 @@ func NewCache(capacity int) Cache {
 		capacity: capacity,
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
+		mutex:    &sync.Mutex{},
 	}
 }
