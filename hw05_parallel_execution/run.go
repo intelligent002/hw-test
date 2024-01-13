87be711d7fcaed93a664761e1ca145d5fdd13c61
Arkady Rosenberg <arkadyr@rewire.to>
2024-01-13 15:04:08 +0200

    initial

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,79 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"fmt"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
-type Task func() error
+type Task func() (string, error)
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
-func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+type counter struct {
+	sync.Mutex
+	threshold    int
+	current      int
+	ignoreErrors bool
+}
+
+func newCounter(threshold int) *counter {
+	return &counter{
+		threshold:    threshold,
+		ignoreErrors: threshold <= 0,
+	}
+}
+
+func (c *counter) increment() {
+	c.Lock()
+	defer c.Unlock()
+
+	c.current++
+}
+
+func (c *counter) reachedThreshold() bool {
+	c.Lock()
+	defer c.Unlock()
+
+	return !c.ignoreErrors && c.current >= c.threshold
+}
+
+func Run(tasks []Task, n int, m int) error {
+	ctr := newCounter(m)
+
+	var wg sync.WaitGroup
+	wg.Add(n)
+
+	tasksChannel := make(chan Task)
+
+	for i := 0; i < n; i++ {
+		go func() {
+			defer wg.Done()
+
+			for task := range tasksChannel {
+				var res, err = task()
+				if err != nil {
+					fmt.Println(err)
+					ctr.increment()
+				} else {
+					fmt.Println(res)
+				}
+			}
+		}()
+	}
+
+	for _, task := range tasks {
+		if ctr.reachedThreshold() {
+			break
+		}
+		tasksChannel <- task
+	}
+
+	close(tasksChannel)
+
+	wg.Wait()
+
+	if ctr.reachedThreshold() {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -12,6 +12,10 @@ import (
 	"go.uber.org/goleak"
 )
 
+func monkey() bool {
+	return rand.Float64() < 0.999
+}
+
 func TestRun(t *testing.T) {
 	defer goleak.VerifyNone(t)
 
@@ -23,10 +27,10 @@ func TestRun(t *testing.T) {
 
 		for i := 0; i < tasksCount; i++ {
 			err := fmt.Errorf("error from task %d", i)
-			tasks = append(tasks, func() error {
+			tasks = append(tasks, func() (string, error) {
 				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
 				atomic.AddInt32(&runTasksCount, 1)
-				return err
+				return "", err
 			})
 		}
 
@@ -49,10 +53,47 @@ func TestRun(t *testing.T) {
 			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
 			sumTime += taskSleep
 
-			tasks = append(tasks, func() error {
+			tasks = append(tasks, func() (string, error) {
 				time.Sleep(taskSleep)
 				atomic.AddInt32(&runTasksCount, 1)
-				return nil
+				return "", nil
+			})
+		}
+
+		workersCount := 5
+		maxErrorsCount := 1
+
+		start := time.Now()
+		err := Run(tasks, workersCount, maxErrorsCount)
+		elapsedTime := time.Since(start)
+		require.NoError(t, err)
+
+		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
+		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
+	})
+
+	t.Run("tasks with 50/50 errors", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+		var sumTime time.Duration
+
+		for i := 0; i < tasksCount; i++ {
+			err := fmt.Errorf("errors from task %d", i)
+			res := fmt.Sprintf("result from task %d", i)
+			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+			sumTime += taskSleep
+
+			tasks = append(tasks, func() (string, error) {
+				time.Sleep(taskSleep)
+
+				atomic.AddInt32(&runTasksCount, 1)
+				if monkey() {
+					return res, nil
+				} else {
+					return "", err
+				}
 			})
 		}
 
